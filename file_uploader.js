718d65867bd7bd231444683183021356d4633d2e
A-Jayakrishna <53211832+A-Jayakrishna@users.noreply.github.com>
2019-08-05 20:56:42 +0530

    Delete file_uploader.js

--- file_uploader.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('index.js', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8090);
